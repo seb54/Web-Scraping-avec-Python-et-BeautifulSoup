c755e535a40b7067bb67bdad6080c2b561c50dea
Sebastien GERARD <sebastiengerard54@gmail.com>
2024-09-30 12:14:37 +0200

    Ajout du fichier requirements + lien pour postuler à chacune des offres

--- env/scraper.py
+++ env/scraper.py
@@ -22,7 +22,7 @@ reset = "\033[0m"  # Réinitialise les styles
 
 # Trouver toutes les div ayant la classe "card-content"
 try:
-    results = soup.find_all(class_="card-contente")
+    results = soup.find_all(class_="card-content")
     if len(results) == 0:  # Vérifier si la liste des offres est vide
         raise ValueError("Aucune offre d'emploi n'a pu être récupérée car aucune div ayant la classe 'card-content' n'a été trouvée")
 except ValueError as e:
@@ -53,12 +53,16 @@ for result in results:
         if job_datetime is None:
             raise ValueError(f"Aucune date trouvée pour l'offre {job_title.text}. Vérifie s'il y a bien une balise <time>")
         
+        job_apply = result.find("a", string=lambda text: "Apply" in text, class_="card-footer-item")
+        if job_apply is None:
+            raise ValueError(f"Aucune lien trouvé pour postuler à l'offre {job_title.text}. Vérifie s'il y a bien un lien <a> ayant l'ancre 'Apply'")
+        
         # Afficher les informations avec des couleurs
         print(f"{bold_start}{blue}Titre du job : {reset}{bold_start}{job_title.text.strip()}{bold_end}")
         print(f"{green}Société : {reset}{job_company.text.strip()}")
         print(f"{yellow}Localisation : {reset}{job_location.text.strip()}")
-        print(f"{gray}Date de publication : {reset}{job_datetime.text.strip()}\n")
-
+        print(f"{gray}Date de publication : {reset}{job_datetime.text.strip()}")
+        print(f"Postuler ici : {blue}{job_apply.get('href')}{reset}\n")
     except ValueError as e:
         print(f"{red}Erreur : {e}{reset}")
         print("Fin du programme")
